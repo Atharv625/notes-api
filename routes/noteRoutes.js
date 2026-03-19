f25c5e8fc9e372d8532da24ea9f1b28a443548ca
Atharv625 <patilatharv625@gmail.com>
2026-03-19 21:18:17 +0530

    first commit

---
+++ routes/noteRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const {
+  createNote,
+  getNotes,
+  getNoteById,
+  updateNote,
+  deleteNote,
+} = require("../controllers/noteController");
+const { protect } = require("../middleware/authMiddleware");
+
+router.post("/", protect, createNote);
+router.get("/", protect, getNotes);
+router.get("/:id", protect, getNoteById);
+router.put("/:id", protect, updateNote);
+router.delete("/:id", protect, deleteNote);
+router.post("/", createNote);
+router.get("/", getNotes);
+router.get("/:id", getNoteById);
+router.put("/:id", updateNote);
+router.delete("/:id", deleteNote);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,13 @@
+const express=require("express");
+const dotenv=require("dotenv");
+const connectDB=require("./config/db");
+dotenv.config();
+connectDB();
+const app=express();
+app.use(express.json());
+app.use("/api/notes", require("./routes/noteRoutes"));
+app.use("/api/auth", require("./routes/authRoutes"));
+const PORT=process.env.PORT||5000;
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`);
+});
